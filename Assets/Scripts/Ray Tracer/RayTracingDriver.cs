93c95a4a06c83e339316cfb71898eb55897c7e2f
Xaphieris <melo0394@kettering.edu>
2025-03-12 00:04:57 -0400

    3/12/25
    Two working ray tracers

--- Assets/Scripts/Ray Tracer/RayTracingDriver.cs
+++ Assets/Scripts/Ray Tracer/RayTracingDriver.cs
@@ -2,9 +2,19 @@ using UnityEngine;
 
 public class RayTracingDriver : MonoBehaviour
 {
+    // Set shader in unity
     public ComputeShader RayTracingShader;
 
+    // Set render target (screen)
     private RenderTexture target;
+
+    // Set custom skybox to reference
+    public Texture SkyboxTexture;
+
+    private uint _currentSample = 0;
+    private Material _addMaterial;
+
+    public Light DirectionalLight;
 
     private void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
@@ -28,13 +38,24 @@ public class RayTracingDriver : MonoBehaviour
         //Debug.Log($"Shader dispatched with threadGroupsX: {threadGroupsX}, threadGroupsY: {threadGroupsY}");
 
         // Blit the result texture to the screen
-        Graphics.Blit(target, destination);
+        if (_addMaterial == null)
+            _addMaterial = new Material(Shader.Find("Hidden/AddShader"));
+        _addMaterial.SetFloat("_Sample", _currentSample);
+        Graphics.Blit(target, destination, _addMaterial);
+        _currentSample++;
+
+        // Blit the result texture to the screen no anti-ailiasing
+        //Graphics.Blit(target, destination);
     }
 
     private void InitRenderTexture()
     {
         if (target == null || target.width != Screen.width || target.height != Screen.height)
         {
+            // Reset samples
+            _currentSample = 0;
+
+
             // Release render texture if we already have one
             if (target != null)
                 target.Release();
@@ -56,10 +77,40 @@ public class RayTracingDriver : MonoBehaviour
         camera = Camera.main;
     }
 
+    // Update
+    private void Update()
+    {
+        // Check if camera has moved, if so, restart sampling
+        if (transform.hasChanged)
+        {
+            _currentSample = 0;
+            transform.hasChanged = false;
+        }
+
+        // Check if the light has changed positions
+        if (DirectionalLight.transform.hasChanged)
+        {
+            _currentSample = 0;
+            DirectionalLight.transform.hasChanged = false;
+        }
+    }
+
     private void SetShaderParameters()
     {
+        // Set camera position and projection matrices
         RayTracingShader.SetMatrix("_CameraToWorld", camera.cameraToWorldMatrix);
         RayTracingShader.SetMatrix("_CameraInverseProjection", camera.projectionMatrix.inverse);
+
+        //Set the skybox
+        RayTracingShader.SetTexture(0, "_SkyboxTexture", SkyboxTexture);
+
+        // Set a random value pixel offset
+        RayTracingShader.SetVector("_PixelOffset", new Vector2(Random.value, Random.value));
+
+        // Set the directional light
+        Vector3 l = DirectionalLight.transform.forward;
+        RayTracingShader.SetVector("_DirectionalLight", new Vector4(l.x, l.y, l.z, DirectionalLight.intensity));
+
 
         //Debug.Log("Camera To World Matrix: " + camera.cameraToWorldMatrix);
         //Debug.Log("Camera Inverse Projection Matrix: " + camera.projectionMatrix.inverse);
